7983a93ddeb47a91c806748bdc5e37fefb75bd16
Semen (Sam) Hzhesiak <brutalsam@gmail.com>
2020-04-20 21:24:34 -0400

    Find median of two sorted arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("dfdfdf");
-        RunLengthEncoding.testSolution();
+        MedianOfTwoSortedArrays.testSolution();
     }
 }
